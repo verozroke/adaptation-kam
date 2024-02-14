e790b7af3612e8b8cd50a1291a7c98ad05719e21
verozroke <73778760+verozroke@users.noreply.github.com>
2024-02-14 11:55:16 +0600

    asdsd

--- components/home/Video.vue
+++ components/home/Video.vue
@@ -3,8 +3,8 @@
     width="500"
     height="300"
     style="border-radius: 20px; margin-bottom: 20px;"
-    src="https://www.youtube.com/embed/fOCWBhuDdDo"
-    title="Exclusive: Tucker Carlson Interviews Vladimir Putin"
+    src="https://www.youtube.com/embed/txt9ijrg-Rs?autoplay=1&mute=1"
+    title="Видео Приветствие КАМ 1"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     allowfullscreen
